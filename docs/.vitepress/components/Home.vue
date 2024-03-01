ee06801a66de221ad01b7662b1476e91e93b3313
王磊 <56986449+NeverSeeG@users.noreply.github.com>
2024-03-01 14:54:02 +0800

    第一次提交

---
+++ docs/.vitepress/components/Home.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme';
+
+const { Layout } = DefaultTheme;
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-image>
+      <img id="hero-image" src="/WechatIMG597.jpg">
+    </template>
+  </Layout>
+</template>
+
+<style scoped>
+img#hero-image {
+  position: relative;
+  min-width: 150%;
+  height: auto;
+  left: -70px;
+  -webkit-mask-image: linear-gradient(to right, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 100%);
+}
+</style>
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @Description：
+ * @Author: 王磊
+ * @Date: 2024-03-01
+ */
+import DefaultTheme from 'vitepress/theme-without-fonts'
+import Home from '../components/Home.vue'
+
+export default {
+    ...DefaultTheme,
+    Layout: Home,
+    enhanceApp({ app }) {
+    }
+}
